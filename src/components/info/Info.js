c739ed6fbe56786693a8b8aed4b574b411edbe8c
GonzaloHollmann <hollmanngonzalo@gmail.com>
2022-09-29 23:27:59 -0300

    add(links proyects)

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -5,7 +5,7 @@ const Info = () => {
   return (
     <div className="info-container">
       <div className="info">
-        <h1>Let's work together and create something unique</h1>
+        <h1>La pasión es el inicio del éxito</h1>
       </div>
     </div>
   );
--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -6,11 +6,13 @@ const slidesInfo = [
     src: "https://images.stockx.com/images/Air-Jordan-1-Retro-High-OG-Stage-Haze-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&trim=color&q=90&dpr=2&updated_at=1653035159",
     alt: "Project 1",
     desc: "Sneaker Paradise",
+    href: "https://front-h4aq04x18-fernando-bernal.vercel.app/"
   },
   {
     src: "https://cdn2.thedogapi.com/images/dW5UucTIW.jpg",
     alt: "Project 2",
     desc: "Dogmania",
+    href: "https://github.com/GonzaloHollmann/DOGMANIA"
   },
   {
     src: "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_960_720.jpg",
@@ -21,7 +23,7 @@ const slidesInfo = [
 
 const slides = slidesInfo.map((slide) => (
   <div className="slide-container">
-    <img src={slide.src} alt={slide.alt} />
+    <a href={slide.href} target="_blank" ><img src={slide.src} alt={slide.alt} /></a>
     <div className="slide-desc">
       <span>{slide.desc}</span>
     </div>
