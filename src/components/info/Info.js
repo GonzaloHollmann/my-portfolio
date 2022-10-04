5de0a5f54cc1537446de6012e7ccb2b13fbd7cf7
GonzaloHollmann <hollmanngonzalo@gmail.com>
2022-10-03 23:35:00 -0300

    fix responsive

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -6,67 +6,67 @@ const Info = () => {
     <div className="info-container">
       <div className="info">
         <div className="iconos">
-            <img
+            <img className="img1"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
               alt="css3"
               width="65"
               height="65"
             />         
-            <img
+            <img className="img2"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
               alt="express"
               width="65"
               height="65"
             />
-            <img
+            <img className="img3"
               src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
               alt="firebase"
               width="65"
               height="65"
             />
-            <img
+            <img className="img4"
               src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg"
               alt="heroku"
               width="65"
               height="65"
             />
-            <img
+            <img className="img5"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
               alt="html5"
               width="65"
               height="65"
             />
-            <img
+            <img className="img6"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
               alt="javascript"
               width="65"
               height="65"
             />
-            <img
+            <img className="img7"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
               alt="mongodb"
               width="65"
               height="65"
             />
-            <img
+            <img className="img8"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
               alt="nodejs"
               width="65"
               height="65"
             />
-            <img
+            <img className="img9"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
               alt="postgresql"
               width="65"
               height="65"
             />
-            <img
+            <img className="img10"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
               alt="react"
               width="65"
               height="65"
             />
-            <img
+            <img className="img11"
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
               alt="redux"
               width="65"
